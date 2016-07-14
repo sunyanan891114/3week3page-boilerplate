307061ebe706ffcec216db9f059b7329ce893254
sun.yanan <sun.yanan@rea-group.com>
2016-07-14 14:46:21 +0800

    Finish 2 week homework.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,30 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var gls = require('gulp-live-server');
+var browserSync = require('browser-sync');
 
 gulp.task('sass', function () {
-  return gulp.src('./src/main.scss')
+  return gulp.src('./src/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
 });
  
 
-gulp.task('serve', function() {
-  //2. serve at custom port
-  var server = gls.static('./', 3030);
-  // var server = gls('./', true, 3030);
-  server.start();
-
-  //use gulp.watch to trigger server actions(notify, start or stop)
-  gulp.watch(['./src/*.scss', './index.html', './src/images/***'], ['sass'], function (file) {
-    server.notify.apply(server, [file]);
+gulp.task('serve',['sass'], function() {
+  browserSync.init({
+    server: "./"
   });
+
+  gulp.watch('./src/*.scss', ['sass']);
+  gulp.watch('./src/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('images', function () {
   return gulp.src('./src/images/**')
       .pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('lib', function () {
+  return gulp.src('./lib/**')
+      .pipe(gulp.dest('./dist/lib'));
 });
